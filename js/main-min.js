a2c7112dc7a44dade36f917db89a53e2bfd30a62
jacism <jk@webfabrikant.de>
2021-10-14 23:23:18 +0200

    hello!

---
+++ js/main-min.js
@@ -0,0 +1,1 @@
+const menuToggle=document.querySelector(".menuButton");menuToggle.addEventListener("click",(()=>{document.body.classList.toggle("menuIsOpen")}));
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector('.menuButton')
+
+menuToggle.addEventListener('click', () => {
+  document.body.classList.toggle('menuIsOpen')
+})
